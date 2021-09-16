8cb33b32c89961230c672a0c5265095f5266e3f3
MurbhyBLG <erwin16@ya.ru>
2021-09-16 20:15:00 +0500

    Done

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -7,31 +7,125 @@ namespace Lab2
     class String
     {
         private List<char> symbols = new List<char>();
+
         public List<char> Symbols { get => symbols; set => symbols = value; }
+
         public String(List<char> vs)
         {
             this.Symbols = vs;
         }
+
         public char this[int idx]
         {
             get => Symbols[idx];
             set => Symbols[idx] = value;
         }
+
         public static String operator +(String a, char c)
         {
             a.Symbols.Add(c);
             return new String(a.Symbols);
         }
 
+        public static bool operator <(String a, String b)
+        {
+            if (a.Symbols.Count >= b.Symbols.Count)
+            {
+                for (int i = 0; i < b.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] < b[i]) return true;
+                    else break;
+                }
+                return false;
+            } 
+            else
+            {
+                for (int i = 0; i < a.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] < b[i]) return true;
+                    else break;
+                }
+                return true;
+            }
+        }
+
+        public static bool operator >(String a, String b)
+        {
+            if (a.Symbols.Count >= b.Symbols.Count)
+            {
+                for (int i = 0; i < b.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] > b[i]) return true;
+                    else break;
+                }
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < a.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] > b[i]) return true;
+                    else break;
+                }
+                return false;
+            }
+        }
+
+        public static bool operator ==(String a, String b)
+        {
+            if (a.Symbols.Count == b.Symbols.Count)
+            {
+                for (int i = 0; i < b.Symbols.Count; i++)
+                {
+                    if (a[i] != b[i]) return false;
+                }
+                return true;
+            }
+            else return false;
+        }
+
+        public static bool operator !=(String a, String b)
+        {
+            if (a.Symbols.Count > b.Symbols.Count)
+            {
+                for (int i = 0; i < b.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] != b[i]) return true;
+                }
+                return true;
+            }
+
+            if (a.Symbols.Count < b.Symbols.Count)
+            {
+                for (int i = 0; i < a.Symbols.Count; i++)
+                {
+                    if (a[i] == b[i]) continue;
+                    if (a[i] != b[i]) return true;
+                }
+                return true;
+            }
+
+            for (int i = 0; i < b.Symbols.Count; i++)
+                if (a[i] != b[i]) return true;
+           
+            return false;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            String s = new String(Console.ReadLine().ToList());
-            Console.WriteLine(s.Symbols.ToArray());
-            s += 'a';
-            Console.WriteLine(s.Symbols.ToArray());
+            String a = new String(Console.ReadLine().ToList());
+            String b = new String(Console.ReadLine().ToList());
+            Console.WriteLine(a < b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
         }
     }
 }
