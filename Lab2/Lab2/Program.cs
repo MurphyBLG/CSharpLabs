9bd89653c1303b888b3c1d4e6a99c3b48bb5ba86
MurbhyBLG <erwin16@ya.ru>
2021-09-16 18:35:08 +0500

    Push back works

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab2
+{
+    class String
+    {
+        private List<char> symbols = new List<char>();
+        public List<char> Symbols { get => symbols; set => symbols = value; }
+        public String(List<char> vs)
+        {
+            this.Symbols = vs;
+        }
+        public char this[int idx]
+        {
+            get => Symbols[idx];
+            set => Symbols[idx] = value;
+        }
+        public static String operator +(String a, char c)
+        {
+            a.Symbols.Add(c);
+            return new String(a.Symbols);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = new String(Console.ReadLine().ToList());
+            Console.WriteLine(s.Symbols.ToArray());
+            s += 'a';
+            Console.WriteLine(s.Symbols.ToArray());
+        }
+    }
+}
